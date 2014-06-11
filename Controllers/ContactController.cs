fa094ca2a483f2c9c94c1c92bd224f994dae0120
Jake Waletzko <jnwaletzko@gmail.com>
2014-06-10 20:48:16 -0500

    Added controllers
    Need to stop cuz kinda raging

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WaletzkoPortfolio.Models;
+
+namespace WaletzkoPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        private PortfolioContext db = new PortfolioContext();
+
+        // GET: /Contact/
+        public ActionResult Index()
+        {
+            return View(db.ContactInfo.ToList());
+        }
+
+        // GET: /Contact/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactInfo contactinfo = db.ContactInfo.Find(id);
+            if (contactinfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactinfo);
+        }
+
+        // GET: /Contact/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Contact/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Name,value")] ContactInfo contactinfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContactInfo.Add(contactinfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactinfo);
+        }
+
+        // GET: /Contact/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactInfo contactinfo = db.ContactInfo.Find(id);
+            if (contactinfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactinfo);
+        }
+
+        // POST: /Contact/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Name,value")] ContactInfo contactinfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactinfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contactinfo);
+        }
+
+        // GET: /Contact/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactInfo contactinfo = db.ContactInfo.Find(id);
+            if (contactinfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactinfo);
+        }
+
+        // POST: /Contact/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ContactInfo contactinfo = db.ContactInfo.Find(id);
+            db.ContactInfo.Remove(contactinfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/PortfolioDetailController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WaletzkoPortfolio.Models;
+
+namespace WaletzkoPortfolio.Controllers
+{
+    public class PortfolioDetailController : Controller
+    {
+        private PortfolioContext db = new PortfolioContext();
+
+        // GET: /PortfolioDetail/
+        public ActionResult Index()
+        {
+            return View(db.PortfolioDetails.ToList());
+        }
+
+        // GET: /PortfolioDetail/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PortfolioDetail portfoliodetail = db.PortfolioDetails.Find(id);
+            if (portfoliodetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(portfoliodetail);
+        }
+
+        // GET: /PortfolioDetail/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /PortfolioDetail/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Description,PictureID")] PortfolioDetail portfoliodetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PortfolioDetails.Add(portfoliodetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(portfoliodetail);
+        }
+
+        // GET: /PortfolioDetail/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PortfolioDetail portfoliodetail = db.PortfolioDetails.Find(id);
+            if (portfoliodetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(portfoliodetail);
+        }
+
+        // POST: /PortfolioDetail/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Description,PictureID")] PortfolioDetail portfoliodetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(portfoliodetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(portfoliodetail);
+        }
+
+        // GET: /PortfolioDetail/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PortfolioDetail portfoliodetail = db.PortfolioDetails.Find(id);
+            if (portfoliodetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(portfoliodetail);
+        }
+
+        // POST: /PortfolioDetail/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PortfolioDetail portfoliodetail = db.PortfolioDetails.Find(id);
+            db.PortfolioDetails.Remove(portfoliodetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
