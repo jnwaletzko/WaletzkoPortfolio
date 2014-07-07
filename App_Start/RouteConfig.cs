521d25b4e2ed472260b3d4e7fc55819d7b913395
Jake Waletzko <jnwaletzko@gmail.com>
2014-07-06 21:55:58 -0500

    Cleaned up some auto-generated files

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WaletzkoPortfolio
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PortfolioDetail", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
